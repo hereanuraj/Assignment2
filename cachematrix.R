f9f008e3c48fb4a2f8a07877edd41f74584415f3
hereanuraj <hereanuraj@gmail.com>
2015-09-25 12:59:10 +0800

    Assignment 2
    Added description

--- cachematrix.R
+++ cachematrix.R
@@ -20,7 +20,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If it is then gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
